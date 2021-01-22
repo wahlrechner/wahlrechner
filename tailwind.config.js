ad91452488c9a0dedf934f8f85f02198ec5f03cb
Linus <linus_koester@t-online.de>
2021-01-22 17:34:32 +0100

    design wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,13 @@ const colors = require("tailwindcss/colors");
 module.exports = {
     purge: ["wahlrechner/**/*.{html,py}"],
     theme: {
-        extend: {},
-        colors: {
-            transparent: "transparent",
-            current: "currentColor",
-            white: "white",
-            gray: colors.gray,
-            red: colors.red,
-            yellow: colors.amber,
-            green: colors.emerald,
-            blue: colors.blue,
-            indigo: colors.indigo,
-            purple: colors.violet,
-            pink: colors.pink,
+        extend: {
+            colors: {
+                gray: colors.gray,
+            },
         },
     },
     variants: {},
     plugins: [],
-    darkMode: "media",
+    darkMode: "class",
 };
