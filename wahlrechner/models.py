6c2c8d2ee4bd1b2568616772d0fac5ff336caae3
Linus <linus_koester@t-online.de>
2021-02-01 14:59:20 +0100

    Django Model

--- wahlrechner/models.py
+++ wahlrechner/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+
+class These(models.Model):
+    these_keyword_help = """<i>Maximal 40 Zeichen</i><br>
+    Ein kurzes Schlagwort, um die These zu beschreiben. Wird in der
+    Seitenleiste des Wahlrechners angezeigt.<br>
+    <b>Beispiel:</b> Klimanotstand"""
+    these_keyword = models.CharField(
+        "Schlagwort", help_text=these_keyword_help, max_length=40)
+
+    these_text_help = """<i>Maximal 400 Zeichen</i><br>
+    Der vollständige Text der These.<br>
+    <b>Beispiel:</b> Die Stadt XYZ sollte den Klimanotstand ausrufen."""
+    these_text = models.TextField(
+        "Vollständige These", help_text=these_text_help, max_length=400)
+
+    these_nr_help = """Die Thesen-Nummer gibt Auskunft über die Reihenfolge, in der alle Thesen
+    angezeigt werden. Dabei wird als erstes die These mit der niedrigsten Thesen-Nummer angezeigt,
+    danach die These mit der jeweils nächst größeren Thesen-Nummer, usw. Die Thesen-Nummer lässt
+    sich also als Priorität für die angezeigte Reihenfolge verstehen, und kann auch eine krumme
+    Zahl sein.<br>
+    <b>Beispiel:</b> Die These, die zuerst angezeigt werden soll, hat die Nummer 1. Die zweite These
+    die Nummer 2, usw."""
+    these_nr = models.FloatField(
+        "Thesen-Nummer", help_text=these_nr_help, unique=True)
+
+    def __str__(self):
+        return self.these_keyword
+
+    class Meta:
+        verbose_name = "These"
+        verbose_name_plural = "Thesen"
