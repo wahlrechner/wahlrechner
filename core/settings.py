f19b60a9483bdc28703238a0e54b35d5bda6000f
Linus <linus_koester@t-online.de>
2021-02-23 16:54:15 +0100

    import-export

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'wahlrechner'
+    'wahlrechner',
+    'import_export'
 ]
 
 MIDDLEWARE = [
--- wahlrechner/admin.py
+++ wahlrechner/admin.py
@@ -1,27 +1,47 @@
 from django.contrib import admin
 from .models import These, Antwort, Partei
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 
 
-class TheseAdmin(admin.ModelAdmin):
+class TheseResource(resources.ModelResource):
+    class Meta:
+        model = These
+
+
+class TheseAdmin(ImportExportModelAdmin):
     list_display = ['these_nr', 'these_keyword', 'these_text']
     list_display_links = ['these_keyword']
     ordering = ['these_nr']
     search_fields = ['these_keyword', 'these_text']
+    resource_class = TheseResource
 
 
-class AntwortAdmin(admin.ModelAdmin):
+class AntwortResource(resources.ModelResource):
+    class Meta:
+        model = Antwort
+
+
+class AntwortAdmin(ImportExportModelAdmin):
     list_display = ['antwort_partei',
                     'antwort_these', 'antwort_position']
     list_display_links = ['antwort_partei', 'antwort_these']
     search_fields = ['antwort_these', 'antwort_partei']
     list_filter = ['antwort_partei']
     autocomplete_fields = ['antwort_these', 'antwort_partei']
+    resource_class = AntwortResource
 
 
-class ParteiAdmin(admin.ModelAdmin):
+class ParteiResource(resources.ModelResource):
+    class Meta:
+        model = Partei
+
+
+class ParteiAdmin(ImportExportModelAdmin):
     search_fields = ['partei_name']
+    resource_class = ParteiResource
 
 
 admin.site.register(These, TheseAdmin)
