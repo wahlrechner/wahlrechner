c3da8e66d19c8dada6b22e7ef6e9260812d9f592
Linus Köster <linus_koester@t-online.de>
2021-04-12 22:31:03 +0200

    Tailwind JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-const colors = require("tailwindcss/colors");
+// const colors = require("tailwindcss/colors");
 
 module.exports = {
+    mode: "jit",
     purge: ["wahlrechner/**/*.{html,py}"],
     theme: {},
     variants: {},
     plugins: [],
-    darkMode: "media",
+    darkMode: "class",
 };
