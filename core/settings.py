54cc42b48802a47d9e3f9161a727b1eeb6daf8db
Linus Köster <linus@crazyeasy.dev>
2022-07-25 17:49:56 +0200

    Alle Django Templates fertig aufgeräumt

--- core/settings.py
+++ core/settings.py
@@ -51,13 +51,21 @@ TEMPLATES = [
         "DIRS": [
             BASE_DIR / f"themes/{THEME}/templates",
         ],
-        "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+            ],
+            "loaders": [
+                (
+                    "django.template.loaders.cached.Loader",
+                    [
+                        "django.template.loaders.filesystem.Loader",
+                        "django.template.loaders.app_directories.Loader",
+                    ],
+                ),
             ],
         },
     },
