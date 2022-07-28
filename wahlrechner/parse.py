f66c3dd428ef77f8bb74c0da7d6ab6c6be35bdd7
Linus Köster <linus@crazyeasy.dev>
2022-07-28 21:54:58 +0200

    Backend neugeschrieben

---
+++ wahlrechner/parse.py
@@ -0,0 +1,253 @@
+import copy
+import os
+
+import numpy
+from django.http.response import Http404
+
+from .models import These, Partei, Antwort
+
+SCHLUESSEL = {
+    "0": [None, None],  # These nicht beantwortet (Null)
+    "1": ["a", False],  # Agree
+    "2": ["d", False],  # Disagree
+    "3": ["n", False],  # Neutral
+    "5": ["a", True],  # Agree (Priorisiert)
+    "6": ["d", True],  # Disagree (Priorisiert)
+    "7": ["n", True],  # Neutral (Priorisiert)
+    "9": ["s", False],  # These übersprungen (Skip)
+}
+
+
+def get_key_from_value(d, val):
+    """Gibt Key für Wert val im Dictionary d zurück."""
+    keys = [k for k, v in d.items() if v == val]
+    if keys:
+        return keys[0]
+    return None
+
+
+def alle_thesen():
+    """Gibt alle Thesen sortiert in einer Liste zurück."""
+    return These.objects.all().order_by("these_nr")
+
+
+def decode_zustand(zustand=0):
+    """Dekodiert die Zustandsinformationen, gibt ein Opinions-Dictionary zurück."""
+
+    # Kodiere den Zustand aus Base-36 in Base-10
+    zustand = int(str(zustand).upper(), 36)
+
+    # Erstelle ein Dictionary mit allen Thesen
+    opinions = dict()
+    for t in These.objects.all():
+        opinions[t] = SCHLUESSEL.get("0").copy()
+
+    for pk, status in enumerate(list(str(zustand))[::-1]):
+
+        # Falls ein Status 4 oder 8 ist, muss es sich um einen ungültigen Zustand handeln
+        if status in ["4", "8"]:
+            raise Http404
+        else:
+            t = These.objects.filter(pk=pk).first()
+
+            if t:
+                opinions[t] = SCHLUESSEL.get(status).copy()
+
+    return opinions
+
+
+def encode_zustand(opinions):
+    """Kodiert die Zustandsinformationen aus einem Opinions-Dictionary, gibt einen Zustand in Base-36 zurück."""
+
+    opinions = list(opinions.items())
+    zustand = 0
+
+    for these, s in opinions:
+        status = get_key_from_value(SCHLUESSEL, s)
+        zustand += int(status) * (10**these.pk)
+
+    zustand = numpy.base_repr(zustand, 36)
+
+    return zustand
+
+
+def generate_zustand(opinions, these, opinion):
+    """Gibt einen Zustand zurück, bei dem die Position bei these zu opinion geändert wurde."""
+
+    opinions = copy.deepcopy(opinions)
+
+    if not (prio := opinions[these][1]) or opinion == "s":
+        prio = False
+
+    opinions[these] = [opinion, prio]
+
+    return encode_zustand(opinions)
+
+
+def thesen_index(thesen, these_current):
+    """Berechnet die Position der aktuellen These im Vergleich zu allen Thesen.
+    Gibt ein Tuple zurück: (aktuelle_pos, max_pos)"""
+    aktuelle_pos = (*thesen,).index(these_current) + 1
+    max_pos = len(thesen)
+
+    return aktuelle_pos, max_pos
+
+
+def these_next(thesen, index):
+    """Gibt das Objekt der nachfolgenden These zurück."""
+    if index[0] < index[1]:
+        return thesen[index[0]]
+    else:
+        return None
+
+
+def these_prev(thesen, index):
+    """Gibt das Objekt der vorherigen These zurück."""
+    if index[0] > 1:
+        return thesen[index[0] - 2]
+    else:
+        return None
+
+
+def update_opinions(opinions, get_dict):
+    """Aktualisiert Opinion-Dictionary: Einträge ohne Positionierung werden als übersprungen markiert,
+    Thesen mit einem passenden GET-Parameter werden als priorisiert markiert."""
+
+    opinions = copy.deepcopy(opinions)
+
+    for these in opinions.keys():
+        if opinions[these] == [None, None]:
+            # Markiere nicht beantwortete Thesen als Übersprungen
+            opinions[these] = ["s", False]
+        else:
+            # Passe Priorisierung entsprechend der GET-Parameter an
+            if get_dict.get(str(these.pk), False):
+                opinions[these] = [opinions[these][0], True]
+            else:
+                opinions[these] = [opinions[these][0], False]
+
+    return encode_zustand(opinions)
+
+
+# noinspection PyGlobalUndefined,PyUnboundLocalVariable
+def calc_result(zustand, opinions):
+    """Berechnet die Übereinstimmung zwischen Nutzer und Parteien."""
+
+    caching_enabled = bool(int(os.getenv("WAHLRECHNER_CACHING", 0)))
+
+    if caching_enabled:
+
+        # Erstelle Cache-Dictionary falls nicht vorhanden
+        if "cache" not in globals():
+            global cache
+            cache = dict()
+
+        # Erhalte Ergebnis falls im Cache
+        if zustand in cache:
+            results = cache.get(zustand)
+
+    # Sonst berechne Ergebnis
+    else:
+        results = []
+
+        for partei in Partei.objects.all():
+            total_p = 0
+            max_p = 0
+
+            for antwort in Antwort.objects.filter(antwort_partei=partei):
+
+                # Position des Nutzers
+                opinion = opinions[antwort.antwort_these][0]
+                prio = opinions[antwort.antwort_these][1]
+
+                # Falls These nicht übersprungen
+                if opinion != "s":
+
+                    # Stimmt Position mit Antwort der Partei überein? (2 Punkte)
+                    if opinion == antwort.antwort_position:
+                        p = 2
+                    # Teilweise Übereinstimmung, Position der Partei oder eigene Position neutral? (1 Punkt)
+                    elif opinion == "n" or antwort.antwort_position == "n":
+                        p = 1
+                    # Keine Übereinstummung (0 Punkte)
+                    else:
+                        p = 0
+
+                    # Ist These vom Nutzer priorisiert? (Doppelte Punkte)
+                    if prio:
+                        total_p += p * 2
+                        max_p += 4  # bei einer wichtigen These sind maximal erreichbare Punkte 4
+                    else:
+                        total_p += p
+                        max_p += 2  # bei einer normalen These sind maximal erreichbare Punkte 2
+
+            if max_p == 0:
+                percentage = 0
+            else:
+                percentage = round((total_p / max_p * 100), 1)
+
+            results.append((partei, percentage))
+
+        # Sortiere die Ergebnisse nach der prozentualen Übereinstimmung
+        results.sort(key=lambda i: i[1], reverse=True)
+
+        # Füge Ergebnis dem Cache hinzu
+        if caching_enabled:
+            cache[zustand] = results
+
+    return results
+
+
+def check_result(opinions):
+    """Überprüft, ob ein Ergebnis aussagekräftig ist."""
+    skips = 0
+
+    for these in These.objects.all():
+        if opinions[these][0] == "s":
+            skips += 1
+
+    if skips > These.objects.all().count() * 0.7:
+        return False
+    else:
+        return True
+
+
+def calc_antworten(zustand, opinions, these):
+    antworten = []
+
+    for partei, _ in calc_result(zustand, opinions):
+        try:
+            antwort = Antwort.objects.get(antwort_these=these, antwort_partei=partei)
+            antworten.append(antwort)
+        except Antwort.DoesNotExist:
+            pass
+
+    return antworten
+
+
+def increase_result_count():
+    """Erhöht den Zähler result_count.txt um 1."""
+
+    # Lese den Zähler aus
+    try:
+        with open("wahlrechner/stats/result_count.txt", "r") as file:
+            result_count = file.read()
+
+        if result_count == "":
+            result_count = 0
+        else:
+            result_count = int(result_count)
+
+    except (FileNotFoundError, ValueError):
+        result_count = 0
+
+    # Erhöhe den Zähler um 1
+    result_count += 1
+
+    # Erstelle die Zähler-Datei, falls sie noch nicht existiert
+    if not os.path.exists("wahlrechner/stats"):
+        os.mkdir("wahlrechner/stats")
+
+    # Aktualisiere den Zähler in der Zähler-Datei
+    with open("wahlrechner/stats/result_count.txt", "w") as file:
+        file.write(str(result_count))
--- wahlrechner/views.py
+++ wahlrechner/views.py
@@ -1,350 +1,106 @@
-import os
-import urllib
-
-from django.http.response import Http404, HttpResponseNotFound, HttpResponseServerError
-from django.shortcuts import get_object_or_404, redirect, render
+from django.http.response import HttpResponseNotFound, HttpResponseServerError
+from django.shortcuts import get_object_or_404, render, redirect
 from django.template.loader import render_to_string
 
-from .models import Antwort, Partei, These
+from .parse import *
 
 
 def start(request):
-    # Erhalte alle Thesen sortiert nach der Thesen-Nummer
-    these_list = These.objects.all().order_by("these_nr")
-
-    return render(request, "wahlrechner/start.html", {"these_list": these_list})
+    context = {"thesen": alle_thesen(), "opinions": decode_zustand()}
+    return render(request, "wahlrechner/start.html", context)
 
 
-def these(request):
-    # Erhalte alle Thesen sortiert nach der Thesen-Nummer
-    these_list = These.objects.all().order_by("these_nr")
+def these(request, these_pk, zustand):
+    these_current = get_object_or_404(These, pk=these_pk)
+    opinions = decode_zustand(zustand)
 
-    # Erhalte these_pk aus Parameter
-    these_pk = request.GET.get("t", "")
-
-    # 404 wenn kein Parameter angegeben
-    if these_pk == "":
-        raise Http404
-
-    # Erhalte Payload aus den Get-Parametern
-    payload = request.GET.copy()
-    del payload["t"]
-    current_payload = urllib.parse.urlencode(payload)
-
-    # Wenn letzte Frage beantwortet, leite zur Gewichtungsseite weiter
-    if these_pk == "c":
-        response = redirect("confirm")
-        response["Location"] += f"?{current_payload}"
-        return response
-
-    # Erhalte Objekt der aktuellen These von PK existiert, sonst 404
-    current_these = get_object_or_404(These, pk=these_pk)
-
-    # Erhalte Index der aktuellen These und Länge aller Thesen
-    pos = (*these_list,).index(current_these)
-    max_pos = len(these_list)
-
-    # Falls letzte These beantwortet, zeige Weiter-Button
-    if str(these_list.reverse()[0].pk) in payload:
-        show_continue_button = True
-    else:
-        show_continue_button = False
-
-    # Aktuelle These beantwortet?
-    if these_pk in payload:
-        these_beantwortet = True
-    else:
-        these_beantwortet = False
-
-    # Payload anpassen für nächste These (nicht bei letzter These)
-    if current_these != these_list.reverse()[0]:
-        # Falls These beantwortet ändere TK um 1
-        payload["t"] = these_list[pos + 1].pk
-        payload_next = urllib.parse.urlencode(payload)
-    else:
-        payload_next = None
-
-    # Payload anpassen für vorherige These (nicht bei erster These)
-    if current_these != these_list[0]:
-        payload["t"] = these_list[pos - 1].pk
-        payload_previous = urllib.parse.urlencode(payload)
-    else:
-        payload_previous = None
-
-    # Payload anpassen auf nächste These, wenn Frage nicht bearbeitet
-    if these_pk not in payload:
-        if current_these == these_list.reverse()[0]:
-            # bei der letzten These ändern für Gewichtungs-Seite
-            payload["t"] = "c"
-        else:
-            next_these = these_list[pos + 1]
-            payload["t"] = next_these.pk
-    else:
-        # Sonst bei aktueller These bleiben
-        payload["t"] = these_pk
-
-    # Payload anpassen für Zustimmung
-    payload[these_pk] = "a"
-    payload_agree = urllib.parse.urlencode(payload)
-
-    # Payload anpassen für Ablehnung
-    del payload[these_pk]
-    payload[these_pk] = "d"
-    payload_disagree = urllib.parse.urlencode(payload)
-
-    # Payload anpassen für Neutral
-    del payload[these_pk]
-    payload[these_pk] = "n"
-    payload_neutral = urllib.parse.urlencode(payload)
-
-    # Payload anpassen für Überspringen
-    del payload[these_pk]
-    payload[these_pk] = "s"
-    payload_skip = urllib.parse.urlencode(payload)
+    thesen = alle_thesen()
+    index = thesen_index(thesen, these_current)
 
     context = {
-        "these_list": these_list,
-        "aktuelle_these": current_these,
-        "pos": pos + 1,
-        "max_pos": max_pos,
-        "current_payload": current_payload,
-        "payload_agree": payload_agree,
-        "payload_disagree": payload_disagree,
-        "payload_neutral": payload_neutral,
-        "payload_skip": payload_skip,
-        "payload_next": payload_next,
-        "payload_previous": payload_previous,
-        "show_continue_button": show_continue_button,
-        "these_beantwortet": these_beantwortet,
+        "opinions": opinions,
+        "index": index,
+        "thesen": thesen,
+        "these_current": these_current,
+        "these_next": these_next(thesen, index),
+        "these_prev": these_prev(thesen, index),
+        "zustand_current": zustand,
+        "zustand_agree": generate_zustand(opinions, these_current, "a"),
+        "zustand_disagree": generate_zustand(opinions, these_current, "d"),
+        "zustand_neutral": generate_zustand(opinions, these_current, "n"),
+        "zustand_skip": generate_zustand(opinions, these_current, "s"),
     }
 
     return render(request, "wahlrechner/these.html", context)
 
 
-def confirm(request):
-    # Erhalte alle Thesen sortiert nach der Thesen-Nummer
-    these_list = These.objects.all().order_by("these_nr")
-
-    # Payload für den Zurück-Knopf
-    payload = request.GET.copy()
-    current_payload = urllib.parse.urlencode(payload)
-    payload["t"] = these_list.reverse()[0].pk
-    payload_previous = urllib.parse.urlencode(payload)
+def confirm(request, zustand):
+    opinions = decode_zustand(zustand)
+    thesen = alle_thesen()
 
     context = {
-        "these_list": these_list,
-        "current_payload": current_payload,
-        "payload_previous": payload_previous,
-        "aussagekraeftig": aussagekraeftig(request),
+        "opinions": opinions,
+        "thesen": thesen,
+        "zustand_current": zustand,
     }
 
     return render(request, "wahlrechner/confirm.html", context)
 
 
-def results(request):
-    # Erhalte alle Thesen sortiert nach der Thesen-Nummer
-    these_list = These.objects.all().order_by("these_nr")
+def confirm_submit(request, zustand):
+    opinions = decode_zustand(zustand)
+    zustand = update_opinions(opinions, request.GET)
+    return redirect("result", zustand)
 
-    # Aktueller Payload
-    payload = request.GET.copy()
-    current_payload = urllib.parse.urlencode(payload)
-    payload["t"] = these_list.reverse()[0].pk
-    payload_previous = urllib.parse.urlencode(payload)
 
-    results = calculate_results(request)
+def result(request, zustand):
+    opinions = decode_zustand(zustand)
+    thesen = alle_thesen()
 
     context = {
-        "these_list": these_list,
-        "current_payload": current_payload,
-        "payload_previous": payload_previous,
-        "results": results,
-        "aussagekraeftig": aussagekraeftig(request),
+        "opinions": opinions,
+        "thesen": thesen,
+        "zustand_current": zustand,
+        "result": calc_result(zustand, opinions),
+        "aussagekraeftig": check_result(opinions),
     }
 
     increase_result_count()
 
-    return render(request, "wahlrechner/results.html", context)
+    return render(request, "wahlrechner/result.html", context)
 
 
-def reason(request):
-    # Erhalte alle Thesen sortiert nach der Thesen-Nummer
-    these_list = These.objects.all().order_by("these_nr")
+def reason(request, these_pk, zustand):
+    these_current = get_object_or_404(These, pk=these_pk)
+    opinions = decode_zustand(zustand)
 
-    # Erhalte Payload aus den Get-Parametern
-    payload = request.GET.copy()
-    del payload["t"]
-    current_payload = urllib.parse.urlencode(payload)
-
-    # Erhalte these_pk aus Parameter
-    these_pk = request.GET.get("t", "")
-
-    # 404 wenn kein Parameter angegeben
-    if these_pk == "":
-        raise Http404
-
-    # Erhalte Objekt der aktuellen These von PK existiert, sonst 404
-    current_these = get_object_or_404(These, pk=these_pk)
-
-    # Erhalte Index der aktuellen These und Länge aller Thesen
-    pos = (*these_list,).index(current_these)
-    max_pos = len(these_list)
-
-    # Payload anpassen für nächste These (nicht bei letzter These)
-    if current_these != these_list.reverse()[0]:
-        # Falls These beantwortet ändere TK um 1
-        payload["t"] = these_list[pos + 1].pk
-        payload_next = urllib.parse.urlencode(payload)
-    else:
-        payload_next = None
-
-    # Payload anpassen für vorherige These (nicht bei erster These)
-    if current_these != these_list[0]:
-        payload["t"] = these_list[pos - 1].pk
-        payload_previous = urllib.parse.urlencode(payload)
-    else:
-        payload_previous = None
-
-    # Berechne Ergebnisse
-    results = calculate_results(request)
-
-    # Erhalte Antworten der Parteien
-    antworten = []
-    for partei, _ in results:
-        try:
-            antwort = Antwort.objects.get(
-                antwort_these=current_these, antwort_partei=partei
-            )
-            antworten.append(antwort)
-        except Antwort.DoesNotExist:
-            pass
+    thesen = alle_thesen()
+    index = thesen_index(thesen, these_current)
 
     context = {
-        "these_list": these_list,
-        "current_payload": current_payload,
-        "pos": pos + 1,
-        "aktuelle_these": current_these,
-        "payload_next": payload_next,
-        "payload_previous": payload_previous,
-        "antworten": antworten,
-        "max_pos": max_pos,
-        "results": results,
+        "opinions": opinions,
+        "index": index,
+        "thesen": thesen,
+        "these_current": these_current,
+        "these_next": these_next(thesen, index),
+        "these_prev": these_prev(thesen, index),
+        "zustand_current": zustand,
+        "antworten": calc_antworten(zustand, opinions, these_current),
     }
 
     return render(request, "wahlrechner/reason.html", context)
-
-
-def calculate_results(request):
-    # Erhalte Cache falls vorhanden, sonst erstelle das globale Dictionary
-    if not "cache" in globals():
-        global cache
-        cache = {}
-
-    # Erhalte Payload und entferne Parameter für die aktuelle These, falls vorhanden
-    payload = request.GET.copy()
-    if "t" in payload:
-        del payload["t"]
-
-    # Sortiere die URL-Parameter und verwandel sie in einen String
-    urlstr = urllib.parse.urlencode(sorted(payload.items()))
-
-    # Erhalte Ergebnis aus Cache, falls vorhanden
-    if urlstr in cache and bool(int(os.environ["WAHLRECHNER_CACHING"])):
-        results = cache.get(urlstr)
-
-    # Sonst berechne Ergebnis
-    else:
-        results = []
-
-        for partei in Partei.objects.all():
-            total_p = 0
-            max_p = 0
-
-            for antwort in Antwort.objects.filter(antwort_partei=partei):
-
-                # Falls These nicht übersprungen
-                if (
-                    position := request.GET.get(str(antwort.antwort_these.pk), "s")
-                ) != "s":
-
-                    # Stimmt Position mit Antwort der Partei überein? (2 Punkte)
-                    if position == antwort.antwort_position:
-                        p = 2
-                    # Teilweise Übereinstimmung, Position der Partei oder eigene Position neutral? (1 Punkt)
-                    elif position == "n" or antwort.antwort_position == "n":
-                        p = 1
-                    # Keine Übereinstummung (0 Punkte)
-                    else:
-                        p = 0
-
-                    # Ist These als wichtig makiert? (Doppelte Punkte)
-                    if request.GET.get(f"p{antwort.antwort_these.pk}", False):
-                        total_p += p * 2
-                        max_p += 4  # bei einer wichtigen These sind maximal erreichbare Punkte 4
-                    else:
-                        total_p += p
-                        max_p += 2  # bei einer normalen These sind maximal erreichbare Punkte 2
-
-            if max_p == 0:
-                percentage = 0
-            else:
-                percentage = round((total_p / max_p * 100), 1)
-
-            results.append((partei, percentage))
-
-        # Sortiere die Ergebnisse nach der prozentualen Übereinstimmung
-        results.sort(key=lambda partei: partei[1], reverse=True)
-
-        # Füge Ergebnis dem Cache hinzu
-        cache[urlstr] = results
-
-    return results
-
-
-def aussagekraeftig(request):
-    skips = 0
-
-    for these in These.objects.all():
-        if request.GET.get(str(these.pk), "s") == "s":
-            skips += 1
-
-    if skips > These.objects.all().count() * 0.6:
-        return False
-    else:
-        return True
 
 
 def handler404(request, exception=""):
     return HttpResponseNotFound(render_to_string("error/404.html"))
 
 
-def handler500(request):
-    return HttpResponseServerError(render_to_string("error/500.html"))
-
-
-def increase_result_count():
-    try:
-        file = open("wahlrechner/stats/result_count.txt", "r")
-        result_count = file.read()
-        file.close()
-        if result_count == "":
-            result_count = 0
-        else:
-            result_count = int(result_count)
-    except (FileNotFoundError, ValueError):
-        result_count = 0
-
-    result_count += 1
-
-    if not os.path.exists("wahlrechner/stats"):
-        os.mkdir("wahlrechner/stats")
-
-    file = open("wahlrechner/stats/result_count.txt", "w")
-    file.write(str(result_count))
-    file.close()
-
-
 def test404(request):
     return render(request, template_name="error/404.html")
+
+
+def handler500(request):
+    return HttpResponseServerError(render_to_string("error/500.html"))
 
 
 def test500(request):
