bc2ba7b6d2f76b93c0f777c2637d17304326ce79
Linus Köster <linus@crazyeasy.dev>
2022-07-31 16:48:55 +0200

    Custom Wahlrechner Title

--- wahlrechner/templatetags/wahlrechner_name.py
+++ wahlrechner/templatetags/wahlrechner_name.py
@@ -3,10 +3,21 @@ import os
 from django.template.defaulttags import register
 
 
+# noinspection PyGlobalUndefined,PyUnboundLocalVariable
 @register.simple_tag(name="wahlrechner_name")
 def wahlrechner_name():
-    if not "w_name" in globals():
+    if "w_name" not in globals():
         global w_name
-        w_name = os.environ["WAHLRECHNER_NAME"]
+        w_name = os.getenv("WAHLRECHNER_NAME")
 
     return w_name
+
+
+# noinspection PyGlobalUndefined,PyUnboundLocalVariable
+@register.simple_tag(name="wahlrechner_title")
+def wahlrechner_title():
+    if "w_title" not in globals():
+        global w_title
+        w_title = os.getenv("WAHLRECHNER_TITLE", default="Startseite")
+
+    return w_title
