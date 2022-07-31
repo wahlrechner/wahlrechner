c9e231f363062e2744dfa05a3d85b271997331a0
Linus Köster <linus@crazyeasy.dev>
2022-07-31 19:44:44 +0200

    Simplified Template Tags

--- wahlrechner/templatetags/wahlrechner_name.py
+++ wahlrechner/templatetags/wahlrechner_name.py
@@ -3,21 +3,11 @@ import os
 from django.template.defaulttags import register
 
 
-# noinspection PyGlobalUndefined,PyUnboundLocalVariable
 @register.simple_tag(name="wahlrechner_name")
 def wahlrechner_name():
-    if "w_name" not in globals():
-        global w_name
-        w_name = os.getenv("WAHLRECHNER_NAME")
-
-    return w_name
+    return os.getenv("WAHLRECHNER_NAME", default="Wahlrechner")
 
 
-# noinspection PyGlobalUndefined,PyUnboundLocalVariable
 @register.simple_tag(name="wahlrechner_title")
 def wahlrechner_title():
-    if "w_title" not in globals():
-        global w_title
-        w_title = os.getenv("WAHLRECHNER_TITLE", default="Startseite")
-
-    return w_title
+    return os.getenv("WAHLRECHNER_TITLE", default="Startseite")
